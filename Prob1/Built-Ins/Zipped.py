bfd9bb9c560384812cd4b90cbda1581d2b691435
deluzAug <deluzenbergeraugusto@gmail.com>
2024-10-10 10:20:24 +0200

    messed up, did not save final file version before comminting

--- Prob1/Built-Ins/Zipped.py
+++ Prob1/Built-Ins/Zipped.py
@@ -1,6 +1,16 @@
-A = [1,2,3]
-B = [6,5,4]
-C = [7,8,9]
-X = [A] + [B] + [C]
-print(X)
-print(zip(*X))
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N, X = map(int, input().split())
+#let's create an empty list that will become our zip object
+students_marks = list()
+
+for _ in range(X):
+    topic_marks = list(map(float, input().split()))
+    #we append the list of votes for topic i
+    students_marks.append(topic_marks)
+    
+#we calculate the average for each tuple created by zip, corresponding to the votes of each student
+avg = [sum(i)/X for i in zip(*students_marks)]
+
+#and then print with one decimal
+for i in range(N):
+    print(f"{avg[i]:.1f}")
